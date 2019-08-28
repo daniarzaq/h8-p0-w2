e4f79430f0fc4df5f5c2c0c2c0eac65772232732
daniarzaq <mdania27@gmail.com>
2019-08-28 17:35:31 +0700

    add exercise 5

---
+++ exercise5.js
@@ -0,0 +1,11 @@
+var word = 'JavaScript';
+var second = 'is';
+var third = 'awesome';
+var fourth = 'and';
+var fifth = 'I';
+var sixth = 'love';
+var seventh = 'it!';
+
+var gabungan = word +' '+ second +' '+  third +' '+  fourth +' '+  fifth +' '+  sixth +' '+  seventh
+
+console.log(gabungan)
