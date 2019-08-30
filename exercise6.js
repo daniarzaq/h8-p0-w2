90e14fc391769e26d09bff0d83ad43ca4adcc708
daniarzaq <mdania27@gmail.com>
2019-08-30 20:06:46 +0700

    add exercise6

---
+++ exercise6.js
@@ -0,0 +1,16 @@
+var limit = 20;
+var deret = 0;
+console.log('Looping Pertama')
+while(deret < limit) { 
+  deret = deret + 2; 
+  console.log(deret + ' - I love coding ')
+}
+
+var limit = 2;
+var deret = 22;
+console.log('Looping Kedua')
+while(deret > limit) { 
+  deret = deret - 2; 
+  console.log(deret + ' - I will become fullstack developer')
+}
+
